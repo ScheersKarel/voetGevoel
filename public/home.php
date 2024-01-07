03c4e3775c62d407fd27c208cc6f530674f2ac2b
ScheersKarel <karel.scheers@student.kdg.be>
2024-01-07 09:56:51 +0100

    made cta and added fonts

--- public/home.php
+++ public/home.php
@@ -10,9 +10,13 @@
     <div class="container">
         <?php include('./components/nav.html'); ?>
 
-        <div class="cta">
-            <h1>Voet Gevoel</h1>
-            <h2>H</h2>
+        <div class="cta" >
+            <div class="cta__content">
+                <h1 class="cta__title">Voet Gevoel</h1>
+                <h2 class="cta__subtitle">Happines is a good reflexology!</h2>
+                <p class="cta__text">Welkom bij Voet Gevoel, een praktijk waar jouw voeten dé hoofdrol spelen want zij zijn de spiegel van je lichaam. Bij Voet Gevoel kan je terecht voor verschillende voetreflex-behandelingen. Je kan bij Voet Gevoel zowel voor een acuut als een chronisch gezondheidsprobleem terecht.</p>
+                <button class="cta__button">MAAK EEN AFSPRAAK</button>
+            </div>
         </div>
     </div>
 </body>
