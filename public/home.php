f36ee0a2c1dc77c941a554500d9b953ef012cf64
ScheersKarel <karel.scheers@student.kdg.be>
2024-01-07 12:29:10 +0100

    content home page made

--- public/home.php
+++ public/home.php
@@ -18,6 +18,128 @@
                 <button class="cta__button">MAAK EEN AFSPRAAK</button>
             </div>
         </div>
+
+        <!-- Wie ben ik -->
+        <div class="about">
+            <div class="about__content">
+                <h2 class="about__content__title">Ik ben Isabel, hier om jou te helpen.</h2>
+                <p class="about__content__text">
+                    Achter Voet Gevoel staat een gepassioneerde voetreflexologe, Isabel Van den Eynde. Met een stevige wetenschappelijke achtergrond en de nodige know-how over voetreflexologie probeer ik je optimaal te helpen.
+                </p>
+                <button class="about__content__button">MEER OVER MIJ</button>
+            </div>
+            <div class="about__img">
+                <img class="about__img__bg" src="./resources/images/dic rectangle.png" alt="">
+                <img class="about__img__isabel" src="./resources/images/img isabel.png" alt="">
+                <img class="about__img__strand" src="./resources/images/img strand.png" alt="">
+                <img class="about__img__hey" src="./resources/images/hey !.png" alt="">
+            </div>
+        </div>
+
+        <!-- treatment -->
+
+        <div class="treatments">
+            <h2 class="treatments__title">Waarmee kan ik helpen?</h2>
+
+            <div class="treatment">
+                <div class="treatment__card">
+                    <img class="treatment__card__img" src="./resources/images/voetreflexologie.png" alt="">
+                    <h3 class="treatment__card__title">Voetreflexologie</h3>
+                    <div class="treatment__card__line"></div>
+                    <p class="treatment__card__text">Een deugddoende behandeling waarbij systematisch alle organen en stelsels op je voeten gestimuleerd worden. Werkt relaxerend, vermindert pijn of doet de pijn verdwijnen, stimuleert je spijsvertering, … Een voetreflex-sessie brengt de zenuwen tot rust.</p>
+                    <button class="treatment__card__button">ONTDEK MEER</button>
+                </div>
+
+                <div class="treatment__card">
+                    <img class="treatment__card__img" src="./resources/images/vertebrele_correctie.png" alt="">
+                    <h3 class="treatment__card__title">Energetische Vertebraal Correctie</h3>
+                    <div class="treatment__card__line"></div>
+                    <p class="treatment__card__text">Een bijzondere correctie van de rug en het bekken. Dit gebeurt via zachte strijkbewegingen op voeten en handen. Zinvol bij allerlei klachten. De behandeling stimuleert tevens de organen en kan toegepast worden bij elke leeftijd. </p>
+                    <button class="treatment__card__button">ONTDEK MEER</button>
+                </div>
+
+                <div class="treatment__card">
+                    <img class="treatment__card__img" src="./resources/images/psychozone_harùonisering.png" alt="">
+                    <h3 class="treatment__card__title">Psychozone harmonisering</h3>
+                    <div class="treatment__card__line"></div>
+                    <p class="treatment__card__text"> Een uiterst zachte en meditatieve aanpak van psychische en emotionele stoornissen. Het is een combinatie van reflexologie en acupressuur. De techniek is ontwikkeld door een Chinese arts en stamt uit de Chinese geneeskunde en visie op gezondheid.</p>
+                    <button class="treatment__card__button">ONTDEK MEER</button>
+                </div>
+        </div>
     </div>
+
+    <!-- customers -->
+
+    <div class="customers">
+        <h2 class="customers__title">Wat mijn klanten zeggen</h2>
+        <!-- Slideshow container -->
+        <div class="slideshow-container">
+
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+                <div class="slider_content">
+                    <div class="numbertext">1 / 3</div>
+                    <img src="./resources/images/img strand.png" style="width:20%">
+                    
+                    <div class="slider_content__content">
+                        <p class="slider_content__content__date">
+                            May 8, 2020
+                        </p>
+                        <h2 class="slider_content__content__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+                        <p class="slider_content__content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="slider_content">
+                    <div class="numbertext">2 / 3</div>
+                    <img src="./resources/images/Rectangle 1.png" style="width:20%">
+                    
+                    <div class="slider_content__content">
+                        <p class="slider_content__content__date">
+                            May 8, 2020
+                        </p>
+                        <h2 class="slider_content__content__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+                        <p class="slider_content__content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="slider_content">
+                    <div class="numbertext">3 / 3</div>
+                    <img src="./resources/images/Rectangle 1 (1).png" style="width:20%">
+                    
+                    <div class="slider_content__content">
+                        <p class="slider_content__content__date">
+                            May 8, 2020
+                        </p>
+                        <h2 class="slider_content__content__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+                        <p class="slider_content__content__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.</p>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+        </div>
+        <br>
+
+        <!-- The dots/circles -->
+        <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+    </div>
+
+</div>
 </body>
 </html>
+<script src="./resources/js/main.js"></script>
