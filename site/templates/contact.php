c40c48342465fb4f90e34053407f3ba3dfdc59a2
ScheersKarel <karel.scheers@student.kdg.be>
2024-01-10 18:15:01 +0100

    put contact in kirby

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -2,53 +2,53 @@
 
 <div class="contact">
 
-    <h1 class="contact__title">Contacteer Mij</h1>
+    <h1 class="contact__title"><?= $page->contactTitle() ?></h1>
 
     <div class="contact__container">
 
         <div class="contact__container__content">
             <h2 class="contact__container__content__title">
-                Graag meer informatie?
+                <?= $page->contactSubtitle() ?>
             </h2>
             <p class="contact__container__content__text">
-                Wilt u graag meer informatie over mijn werkwijze, mogelijkheden of tarieven? Neem gerust vrijblijvend contact op. Ik informeer u graag over wat ik voor u kan betekenen. U kan natuurlijk ook een afspraak maken via het formulier of telefonisch. <br><br>U kan mij bereiken op:
+                <?= $page->contactText() ?>
             </p>
 
-            <a href="tel:0485604888">
+            <a href="tel:<?= $page->number() ?>">
                 <div class="contact__container__content__info">
                     <iconify-icon class="contact__container__content__info__icon" icon="iconoir:phone" style="color: white;" width="20" height="20"></iconify-icon>
-                    <p class="contact__container__content__info__text">0485 60 48 88</p>
+                    <p class="contact__container__content__info__text"><?= $page->number() ?></p>
                 </div>
             </a>
 
-            <a href="mailto:isabel.vandeneynde@hotmail.com">
+            <a href="mailto:<?= $page->email() ?>">
                 <div class="contact__container__content__info">
                     <iconify-icon class="contact__container__content__info__icon" icon="material-symbols:mail-outline" style="color: white;" width="20" height="20"></iconify-icon>
-                    <p class="contact__container__content__info__text">isabel.vandeneynde@hotmail.com</p>
+                    <p class="contact__container__content__info__text"><?= $page->email() ?></p>
                 </div>
             </a>
 
             <a href="https://www.google.com/maps/place/Herenboslaan+12A,+2500+Lier/@51.0950966,4.6052671,17z/data=!3m1!4b1!4m6!3m5!1s0x47c3fd2e941050e1:0x6f4db5f2aa4e8abc!8m2!3d51.0950933!4d4.607842!16s%2Fg%2F11krgxxqq6?entry=ttu">
                 <div class="contact__container__content__info">
                     <iconify-icon class="contact__container__content__info__icon" icon="carbon:location" style="color: white;" width="20" height="20"></iconify-icon>
-                    <p class="contact__container__content__info__text">Herenboslaan 12A, 2500 Lier</p>
+                    <p class="contact__container__content__info__text"><?= $page->address() ?></p>
                 </div>
             </a>
 
 
             <div class="contact__container__content__line"></div>
 
-            <a href="https://www.instagram.com/voet_gevoel/">
+            <a href="<?= $page->intagramUrl() ?>">
                 <div class="contact__container__content__info">
                     <iconify-icon class="contact__container__content__info__icon" icon="streamline:instagram-solid" style="color: white;" width="20" height="20"></iconify-icon>
-                    <p class="contact__container__content__info__text">Voet Gevoel</p>
+                    <p class="contact__container__content__info__text"><?= $page->instagram() ?></p>
                 </div>
             </a>
 
-            <a href="https://www.facebook.com/IsabelVoetGevoel?locale=nl_NL">
+            <a href="<?= $page->facebookUrl() ?>">
                 <div class="contact__container__content__info">
                     <iconify-icon class="contact__container__content__info__icon" icon="ri:facebook-fill" style="color: white;" width="20" height="20"></iconify-icon>
-                    <p class="contact__container__content__info__text">Voet gevoel</p>
+                    <p class="contact__container__content__info__text"><?= $page->facebook() ?></p>
                 </div>
             </a>
 
@@ -56,7 +56,7 @@
 
             <div class="contact__container__content__info">
                 <iconify-icon class="contact__container__content__info__icon" icon="mdi:paper-outline" style="color: white;" width="20" height="20"></iconify-icon>
-                <p class="contact__container__content__info__text">BE19 9734 5653 8012</p>
+                <p class="contact__container__content__info__text"><?= $page->rekeningnummer() ?></p>
             </div>
 
 
@@ -64,7 +64,7 @@
 
         <div class="contact__container__formulier">
             <h2 class="contact__container__formulier__title">
-                Of stuur een berichtje!
+                <?= $page->formTitle() ?>
             </h2>
             <form action="https://formsubmit.co/karel.scheers@telenet.be" method="POST">
                 <div class="contact__container__formulier__form">
