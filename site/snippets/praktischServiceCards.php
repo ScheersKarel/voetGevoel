51619bc2a24a838d33f775c106210a4918c3ffa1
ScheersKarel <karel.scheers@student.kdg.be>
2024-01-15 13:25:43 +0100

    praktisch servicecard dynamisch gemaakt

--- site/snippets/praktischServiceCards.php
+++ site/snippets/praktischServiceCards.php
@@ -7,72 +7,29 @@
 
 <div class="praktisch_service_cards">
 
-    <div class="service_card">
-        <h2 class="service_card__title">Voetreflexologie preventief en curatief</h2>
-        <h3 class="service_card__subtitle">Wat kan je verwachten?</h3>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Stimuleert herstellend vermogen van je lichaam</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Heilzaam voor organen en stelsels</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Relaxerend en ontspannend</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Ontgiftende werking</p>
-        </div>
-        <a href="behandelingen/voetreflexologie"><button class="service_card__button">ONTDEK MEER</button></a>
+    <?php foreach ($page->serviceCard()->toStructure() as $item) : ?>
+        <div class="service_card">
 
-    </div>
+            <h2 class="service_card__title"><?= $item->title() ?></h2>
+            <h3 class="service_card__subtitle"><?= $item->subtitle() ?></h3>
+            <div class="service_card__info">
+                <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
+                <p class="service_card__info__text"><?= $item->text1() ?></p>
+            </div>
+            <div class="service_card__info">
+                <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
+                <p class="service_card__info__text"><?= $item->text2() ?></p>
+            </div>
+            <div class="service_card__info">
+                <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
+                <p class="service_card__info__text"><?= $item->text3() ?></p>
+            </div>
+            <div class="service_card__info">
+                <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
+                <p class="service_card__info__text"><?= $item->text4() ?></p>
+            </div>
+            <a href="<?= $item->buttonUrl() ?>"><button class="service_card__button"><?= $item->button() ?></button></a>
 
-    <div class="service_card">
-        <h2 class="service_card__title">Energetische Vertebraal Correctie</h2>
-        <h3 class="service_card__subtitle">Wat kan je verwachten?</h3>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Zeer heilzaam bij rugklachten</p>
         </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Zachte en aangename behandeling</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Snel zichtbare en voelbare resultaten</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Inzetbaar bij diverse problemen of pijnen</p>
-        </div>
-        <a href="behandelingen/vertebraal-correctie"><button class="service_card__button">ONTDEK MEER</button></a>
-
-    </div>
-
-    <div class="service_card">
-        <h2 class="service_card__title">Psychozone Harmonisering</h2>
-        <h3 class="service_card__subtitle">Wat kan je verwachten?</h3>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Combinatie van reflexologie en acupressuur</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Heilzaam bij diverse psychische en emotionele problemen</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Relaxerend en ontgiftend</p>
-        </div>
-        <div class="service_card__info">
-            <iconify-icon class="service_card__info__icon" icon="simple-line-icons:check" style="color:#BF4D1B;" width="30" height="30"></iconify-icon>
-            <p class="service_card__info__text">Zelftherapie mogelijk dankzij acupressuurpunten</p>
-        </div>
-        <a href="behandelingen/psychozone-harmonisering"><button class="service_card__button">ONTDEK MEER</button></a>
-
-    </div>
+    <?php endforeach ?>
 </div>
